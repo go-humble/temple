45c24cd9a1e1bea8488890440e415f32bb5fe300
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-06 16:54:28 -0400

    Add support for includes flag

--- main.go
+++ main.go
@@ -27,20 +27,22 @@ func init() {
 }
 
 func main() {
-	var cmdBuild = &cobra.Command{
+	cmdBuild := &cobra.Command{
 		Use:   "build <src> <dest>",
 		Short: "Compile the templates in the src directory into a package in the dest directory.",
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) != 2 {
 				prtty.Error.Fatal("temple build requires exactly 2 arguments: the src directory and the dest directory.")
 			}
-			if err := build(args[0], args[1]); err != nil {
+			includes := cmd.Flag("includes").Value.String()
+			if err := build(args[0], args[1], includes); err != nil {
 				prtty.Error.Fatal(err)
 			}
 		},
 	}
+	cmdBuild.Flags().String("includes", "", "A directory which includes *.tmpl files to be shared between all templates.")
 
-	var cmdVersion = &cobra.Command{
+	cmdVersion := &cobra.Command{
 		Use:   "version",
 		Short: "Print the current version number.",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -48,7 +50,7 @@ func main() {
 		},
 	}
 
-	var rootCmd = &cobra.Command{
+	rootCmd := &cobra.Command{
 		Use:   "temple",
 		Short: "A command line tool for sharing go templates between client and server.",
 	}
@@ -60,39 +62,79 @@ func main() {
 
 type TemplateData struct {
 	PackageName string
-	Templates   []TemplateFile
+	Templates   []*TemplateFile
+	Includes    []*TemplateFile
 }
 
 type TemplateFile struct {
-	Name   string
-	Source string
+	VarName string
+	Name    string
+	Source  string
 }
 
-func build(src, dest string) error {
+func NewTemplateFile(filename string) (*TemplateFile, error) {
+	// name is everything after the last slash, not including the file extension
+	name := strings.TrimSuffix(filename[strings.LastIndex(filename, string(os.PathSeparator))+1:], ".tmpl")
+	// varName is just the name titlized so it is an exported variable
+	varName := strings.Title(name)
+	fileContents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &TemplateFile{
+		VarName: varName,
+		Name:    name,
+		Source:  string(fileContents),
+	}, nil
+}
+
+func ParseTemplateFiles(dir string) ([]*TemplateFile, error) {
+	templateFiles := []*TemplateFile{}
+	files, err := filepath.Glob(filepath.Join(dir, "*.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+	for _, filename := range files {
+		prtty.Default.Printf("    %s", filename)
+		tf, err := NewTemplateFile(filename)
+		if err != nil {
+			return nil, err
+		}
+		templateFiles = append(templateFiles, tf)
+	}
+	return templateFiles, nil
+}
+
+func build(src, dest, includes string) error {
 	prtty.Info.Println("--> building...")
 	prtty.Default.Printf("    src: %s", src)
 	prtty.Default.Printf("    dest: %s", dest)
+	if includes != "" {
+		prtty.Default.Printf("    includes: %s", includes)
+	}
 
 	// packageName is everything after the last path separator
-	packageName := dest[strings.LastIndex(dest, string(os.PathSeparator)):]
+	packageName := dest[strings.LastIndex(dest, string(os.PathSeparator))+1:]
 	templateData := TemplateData{
 		PackageName: packageName,
 	}
-	srcFiles, err := filepath.Glob(src + string(os.PathSeparator) + "*.tmpl")
-	if err != nil {
-		return err
-	}
-	for _, file := range srcFiles {
-		contents, err := ioutil.ReadFile(file)
+
+	if includes != "" {
+		prtty.Info.Println("--> parsing includes...")
+		includes, err := ParseTemplateFiles(includes)
 		if err != nil {
 			return err
 		}
-		templateData.Templates = append(templateData.Templates, TemplateFile{
-			Name:   strings.Title(strings.TrimPrefix(strings.TrimSuffix(file, ".tmpl"), src+"/")),
-			Source: string(contents),
-		})
+		templateData.Includes = includes
 	}
+	prtty.Info.Println("--> parsing template files...")
+	templates, err := ParseTemplateFiles(src)
+	if err != nil {
+		return err
+	}
+	templateData.Templates = templates
 
+	prtty.Info.Println("--> generating go code...")
 	if err := os.MkdirAll(dest, os.ModePerm); err != nil {
 		return err
 	}
@@ -107,6 +149,5 @@ func build(src, dest string) error {
 	}
 
 	prtty.Info.Println("--> done")
-
 	return nil
 }
