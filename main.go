db5fd045f8eb02705da0d4ce3b66e9d7297f5d3e
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-06 22:34:13 -0400

    Add support for layouts.

--- main.go
+++ main.go
@@ -25,18 +25,78 @@ var (
 func main() {
 	cmdBuild := &cobra.Command{
 		Use:   "build <src> <dest>",
-		Short: "Compile the templates in the src directory into a package in the dest directory.",
+		Short: "Compile the templates in the src directory into a go file in the dest directory.",
+		Long: `The build command will compile the .tmpl files found in the src directory,
+
+along with the .tmpl files found in the layouts and includes directories (if
+provided), and write the results to a go file in the dest directory.
+
+The build command works best if your templates are organized to approximate
+template inheritance, as described in this article:
+https://elithrar.github.io/article/approximating-html-template-inheritance/.
+However, if you don't want organize your templates this way, the compiled go
+file will give you direct access to the builtin html templates
+(*template.Template objects from the html/template package), so you can combine
+parse trees manually. You also have the option of not combining parse trees at
+all, and simply having each .tmpl file represent a stand-alone template.
+
+The generated go file is designed to be fairly readable for humans, so feel
+free to take a look. (Just don't edit it directly!)
+
+## Includes
+
+If provided, all .tmpl files in the includes directory are referred to as
+"includes templates" or simply "includes". Includes are parsed first (before
+layouts and regular templates). Includes should contain .tmpl files for things
+like the <head> section, which are shared between different layouts, or other
+components that are shared between different regular templates. No .tmpl file in
+the includes directory can conflict with any other .tmpl file (e.g. they cannot
+declare sub-templates of the same name). All the includes will be added to the
+parse tree for the layouts and all other templates via the template.AddParseTree
+method. It is safe for includes to reference each other, as long as they don't
+conflict or create cyclical references.
+
+## Layouts
+
+If provided, all .tmpl files in the layouts directory are referred to as "layout
+templates" or simply "layouts". Layouts are parsed after includes and before
+regular templates. Typically, layouts will be referenced by a regular template,
+and will expect the regular template to define certain sub-templates (e.g.
+"content" or "title"), which will then be inserted into the layout. An
+application will almost always want to have at least one layout, conventially
+called "app.tmpl", which regular templates will use. No .tmpl file in the
+layouts directory can conflict with any other .tmpl file (e.g. they cannot
+declare sub-templates of the same name). If includes were also provided, all
+includes will be added to the parse tree for each layout via the
+template.AddParseTree method. Therefore a layout can reference any template in
+includes. Layouts can also reference each other, as long as they don't conflict
+or create cylclical references. All layouts will be added to the parse tree for
+the regular templates in the src directory via the template.AddParseTree method.
+
+## Regular Templates
+
+All the .tmpl files found in the src directory are referred to as "regular
+templates", or simply "templates", and are parsed last. All layouts and includes
+(if any) are added to the parse tree for each template via the
+template.AddParseTree method. Therefore templates can reference both layouts and
+includes. Since regular templates will never be parsed together, they can
+conflict with eachother (e.g. they can declare sub-templates of the same name).
+As a consequence, regular templates also cannot reference eachother.
+
+`,
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) != 2 {
 				prtty.Error.Fatal("temple build requires exactly 2 arguments: the src directory and the dest directory.")
 			}
 			includes := cmd.Flag("includes").Value.String()
-			if err := build(args[0], args[1], includes); err != nil {
+			layouts := cmd.Flag("layouts").Value.String()
+			if err := build(args[0], args[1], includes, layouts); err != nil {
 				prtty.Error.Fatal(err)
 			}
 		},
 	}
-	cmdBuild.Flags().String("includes", "", "A directory which includes *.tmpl files to be shared between all templates.")
+	cmdBuild.Flags().String("includes", "", "(optional) The directory to look for includes. Includes are .tmpl files that are shared between layouts and all templates.")
+	cmdBuild.Flags().String("layouts", "", "(optional) The directory to look for layouts. Layouts are .tmpl shared between all templates and have access to includes.")
 
 	cmdVersion := &cobra.Command{
 		Use:   "version",
@@ -60,6 +120,7 @@ type TemplateData struct {
 	PackageName string
 	Templates   []*TemplateFile
 	Includes    []*TemplateFile
+	Layouts     []*TemplateFile
 }
 
 type TemplateFile struct {
@@ -101,15 +162,17 @@ func ParseTemplateFiles(dir string) ([]*TemplateFile, error) {
 	return templateFiles, nil
 }
 
-func build(src, dest, includes string) error {
+func build(src, dest, includes, layouts string) error {
 	prtty.Info.Println("--> building...")
 	prtty.Default.Printf("    src: %s", src)
 	prtty.Default.Printf("    dest: %s", dest)
 	if includes != "" {
 		prtty.Default.Printf("    includes: %s", includes)
 	}
+	if layouts != "" {
+		prtty.Default.Printf("    layouts: %s", layouts)
+	}
 
-	// packageName is everything after the last path separator
 	packageName := filepath.Base(dest)
 	templateData := TemplateData{
 		PackageName: packageName,
@@ -123,7 +186,15 @@ func build(src, dest, includes string) error {
 		}
 		templateData.Includes = includes
 	}
-	prtty.Info.Println("--> parsing template files...")
+	if layouts != "" {
+		prtty.Info.Println("--> parsing layouts...")
+		layouts, err := ParseTemplateFiles(layouts)
+		if err != nil {
+			return err
+		}
+		templateData.Layouts = layouts
+	}
+	prtty.Info.Println("--> parsing templates...")
 	templates, err := ParseTemplateFiles(src)
 	if err != nil {
 		return err
