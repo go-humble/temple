114917fd692630a6a96e7bd026967d731cc9d569
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-15 23:06:12 -0400

    Write a test for Build which generates and then go runs a file.

---
+++ temple/build_test.go
@@ -0,0 +1,28 @@
+package temple
+
+import (
+	"os/exec"
+	"testing"
+)
+
+const (
+	destFile = "test_files/templates.go"
+	runFile  = "test_files/run.go"
+)
+
+func TestBuild(t *testing.T) {
+	// Generate a go source file with build
+	if err := Build("test_files/templates", destFile, "test_files/partials", "test_files/layouts", "main"); err != nil {
+		t.Error(err)
+	}
+	// Use go run to run the file together with the run file
+	cmd := exec.Command("go", "run", destFile, runFile)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Error(err)
+	}
+	expected := "<html><head><title>Todos</title></head><body><ul><li>One</li><li>Two</li><li>Three</li></ul></body></html>"
+	if string(output) != expected {
+		t.Errorf("Output from generated code was not correct.\nExpected %s\nBut got:  %s", expected, string(output))
+	}
+}
---
+++ temple/test_files/run.go
@@ -0,0 +1,29 @@
+// NOTE: this file is meant to be run together with a generated file created
+// by Build. By itself it won't compile because Templates is not defined.
+
+package main
+
+import (
+	"log"
+	"os"
+)
+
+type Todo struct {
+	Title string
+}
+
+var todos = []Todo{
+	{Title: "One"},
+	{Title: "Two"},
+	{Title: "Three"},
+}
+
+func main() {
+	todosTmpl, found := Templates["todos/index"]
+	if !found {
+		log.Fatal(`Template named "todos/index" was not added to map of Templates`)
+	}
+	if err := todosTmpl.Execute(os.Stdout, todos); err != nil {
+		log.Fatal(err)
+	}
+}
