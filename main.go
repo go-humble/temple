0ab5d1462a37a0a5b3b6b0a11a07c480e61fbd19
Alex Browne <stephenalexbrowne@gmail.com>
2015-05-28 21:43:12 -0400

    Update version to 0.1.0

--- main.go
+++ main.go
@@ -8,7 +8,7 @@
 // code which is compatible with gopherjs and can be compiled to
 // javascript to run in the browser.
 //
-// Version X.X.X (develop)
+// Version 0.1.0
 package main
 
 import (
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	version = "temple version X.X.X (develop)"
+	version = "temple version 0.1.0"
 )
 
 var (
